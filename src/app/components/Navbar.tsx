a9b4eb96f43f2f95c9ca37cf2c0d64e255823730
Levisawada <cdlzhm@gmail.com>
2024-12-22 17:40:59 -0700

    release/cards Cards crops UI created

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,18 +1,18 @@
-import Image from "next/image"
+import Image from 'next/image';
 
 //Styles
-import "../css/navbar.css"
+import '../css/navbar.css';
 
 export default function Navbar() {
-    return(
-    <nav className="navbar__container">
-        <Image
-            src="/profile-picture-default.png"
-            alt="Profile pic"
-            width={40}
-            height={40}
-        />
-        <h1 className="text-2xl font-bold">Welcome, User</h1>
-    </nav>
-    )
+    return (
+        <nav className="navbar__container">
+            <Image
+                src="/profile-picture-default.png"
+                alt="Profile pic"
+                width={40}
+                height={40}
+            />
+            <h1 className="text-2xl font-bold">Welcome, User</h1>
+        </nav>
+    );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-import Navbar from './components/Navbar'
+import './css/card-container.css';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
 
 export default function Home() {
-  return (
-    <div className="container max-w-full p-4">
-      <Navbar></Navbar>
-    </div>
-  );
+    return (
+        <div className="container max-w-full h-screen p-4 grid grid-rows-[auto_1fr]">
+            <Navbar></Navbar>
+            {/* Card´s container */}
+            <section className="card-container p-4">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </section>
+        </div>
+    );
 }
